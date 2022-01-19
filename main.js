037674e3a13ecf4a5b10e61fbc4888b2a41a0376
BaekJiwan <bn1357nb@gmail.com>
2022-01-20 00:01:51 +0900

    220109_Update_success 13_clean_topic1

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var url = require('url');
 var qs = require('querystring');
-var template = require('./lib/template.js');
-var db = require('./lib/db.js');
+var template = require('./lib/template');
+var db = require('./lib/db');
+var topic = require('./lib/topic');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -12,17 +13,7 @@ var app = http.createServer(function(request,response){
         if(queryData.id === undefined){//쿼리데이터의 아이디가 없다면 홈화면이다. welcome
             //db변수의 쿼리메소드의 첫인자로 sql구문을 보내고 두번째 인자로 콜백함수 -> 콜백함수의 첫 인자로 에러문,두번째 인자로
             //토픽을 받아올것이다.(테이블).
-            db.query(`SELECT * FROM topic`,function(error,topics){
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = template.list(topics);
-                var html = template.HTML(title, list,
-                    `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>`
-                );
-                response.writeHead(200);
-                response.end(html);
-            });
+            topic.home(request,response);
         } else { //홈화면이 아니라면 글을 클릭한이후 상세보기이다.
             db.query(`SELECT * FROM topic`,function(error,topics){
                 if(error){
